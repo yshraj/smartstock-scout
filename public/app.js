c77f36dc499dae009289ab424154fb416f82c152
Yash <yashdarjicse@gmail.com>
2025-05-01 12:06:24 +0530

    railway update

--- public/app.js
+++ public/app.js
@@ -42,8 +42,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const API_BASE_URL = hostname === ''
     ? 'http://localhost:3000'
-    : 'https://smartstock-scout.onrender.com';
-  
+    //: 'https://smartstock-scout.onrender.com';
+    : 'https://smartstock-scout-production.up.railway.app/';
 
   async function fetchStocks() {
     try {
